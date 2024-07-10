89de09e9ea9d8eb40387ac59551dc97e04c4b208
Mateus Elias <meap@cin.ufpe.br>
2024-07-09 22:57:00 -0300

    style(backend/app.py): apply black formatter

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app)
 
+
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
