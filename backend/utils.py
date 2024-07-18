29a5d20251e126b880fad8470bde30d99a309275
Mateus Elias <meap@cin.ufpe.br>
2024-07-18 07:11:10 -0300

    feat: implement second version  (#21)

--- backend/utils.py
+++ backend/utils.py
@@ -1,24 +1,56 @@
-def generate_exam(client, context):
-    try:
-        response = client.completions.create(
-            model="gpt-3.5-turbo-instruct",
-            prompt=f"""
-            Para criar uma prova de múltipla escolha sobre o seguinte tema:
-            
-            {context}
-            
-            Por favor, inclua perguntas variadas que avaliem diferentes aspectos do tema. Utilize uma linguagem clara e objetiva nas questões e certifique-se de que cada pergunta tenha apenas uma resposta correta.
+def generate_prompt(
+    num_questions,
+    question_type,
+    subject,
+    difficulty,
+    additional_info,
+    context_text,
+    context_restriction,
+):
+    prompt = ""
+    if context_text:
+        prompt += f"Use the following context as the primary source of information.\nContexto: {context_text}\n"
+        prompt += 'The student will not have access to the context during the test, but he studied it previously. Do not make explicit reference to the context, for example "According to the text", as the text will not be in the test, but the answers must be in accordance with the context.\n'
+        prompt += "If there are conflicting details, make a note at the end but prioritize the content in the context provided.\n"
 
-            Pergunta:
-            """,
-            max_tokens=1024,
-            temperature=0.7,
-            top_p=1.0,
-            stop="\n\nPergunta:",
-        )
+    prompt += f"Generate an exam with the following parameters:\n"
+    if subject:
+        prompt += f"- Subject: {subject}\n"
+    if question_type:
+        prompt += f"- Question type: {question_type}\n"
+    if num_questions:
+        prompt += f"- Number of questions: {num_questions}\n"
+    if difficulty:
+        prompt += f"- Difficulty level: {difficulty}\n"
 
-        questions = response.choices[0].text
-        return questions
+    if additional_info:
+        prompt += "Prioritize user instructions over the predefined parameters.\n"
+        prompt += f"\nUser instructions:\n{additional_info}\n"
 
-    except Exception as e:
-        raise ValueError(f"Error generating exam: {str(e)}")
+    prompt += "Add the answers key and correction instructions to the end.\n"
+    prompt += "The output must be in markdown format, divided into the following optional sections: subject, instructions for solving the questions, questions, answer keys (contains the complete answer for the questions), correction instructions (for example, if the question type is essay, highlight important topics in detail to help the teacher make an objective correction), and alerts."
+
+    if context_restriction:
+        prompt += "Restrict to the content of the context provided.\n"
+    else:
+        prompt += "Use the context content provided, but don't limit to it.\n"
+
+    prompt += (
+        "Generate in the same language as the context, user instructions or subject"
+    )
+
+    return prompt
+
+
+def generate_exam(client, prompt):
+    response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "You are an expert in creating exam questions.",
+            },
+            {"role": "user", "content": prompt},
+        ],
+        model="gpt-3.5-turbo",
+    )
+    return response.choices[0].message.content
--- frontend/src/pages/Results/index.tsx
+++ frontend/src/pages/Results/index.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import { useLocation } from "react-router-dom";
+import ExamRenderer from "../../components/ExamRenderer";
 
 export default function Results() {
   const location = useLocation();
@@ -21,27 +22,7 @@ export default function Results() {
       <section className="pt-24 flex flex-col items-center justify-center lg:px-48 md:px-12 px-4 bg-secondary">
         <h2 className="secondaryTitle bg-underline3 bg-100%">Prova gerada</h2>
         <div className="p-4">
-          {examData &&
-            examData.map((questionData: any, index: any) => (
-              <div key={index} className="mb-8">
-                <h3 className="text-lg font-semibold mb-2">
-                  {questionData.question}
-                </h3>
-                <ul className="list-disc ml-6">
-                  {questionData.options.map((option: any) => (
-                    <li key={option}>
-                      <span>{option}</span>
-                    </li>
-                  ))}
-                </ul>
-                {questionData.correctAnswer && (
-                  <p className="mt-2">
-                    <strong>Resposta correta: </strong>
-                    {questionData.correctAnswer}
-                  </p>
-                )}
-              </div>
-            ))}
+          {examData && <ExamRenderer content={examData} />}
         </div>
       </section>
       <Footer />
