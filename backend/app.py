e3e9000d07c4a8ea21c4fec252bfec7229188bcd
Mateus Elias <meap@cin.ufpe.br>
2024-07-18 06:59:01 -0300

    feat(backend): implement first version

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from openai import OpenAI
-from utils import generate_exam
+from utils import generate_prompt, generate_exam
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -15,15 +15,28 @@ def hello_world():
 @app.route("/api/generate-exam", methods=["POST"])
 def receive_data():
     data = request.get_json()
-    context = data.get("context")
+    num_questions = data.get("number_of_questions", None)
+    question_type = data.get("questions_type", None)
+    subject = data.get("subject", None)
+    difficulty = data.get("difficulty", None)
+    additional_info = data.get("additional_information", None)
+    context_restriction = data.get("context_restriction")
+    context_file = data.get("context", None)
+
     client = OpenAI(api_key=data.get("api_key"))
+    prompt = generate_prompt(
+        num_questions,
+        question_type,
+        subject,
+        difficulty,
+        additional_info,
+        context_file,
+        context_restriction,
+    )
+    exam = generate_exam(client, prompt)
 
     try:
-        questions = generate_exam(client, context)
-
-        return jsonify(
-            {"message": "Exam generated successfully!", "questions": questions}
-        )
+        return jsonify({"message": "Exam generated successfully!", "exam": exam})
 
     except ValueError as ve:
         return jsonify({"error": str(ve)}), 500
