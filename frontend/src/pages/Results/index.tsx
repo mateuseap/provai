9d20633a6d87f7be13a177ade3d211343f705beb
Mateus Elias <meap@cin.ufpe.br>
2024-07-25 07:06:48 -0300

    feat(frontend): improve the exam display

--- frontend/src/pages/Results/index.tsx
+++ frontend/src/pages/Results/index.tsx
@@ -3,16 +3,17 @@ import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import { useLocation } from "react-router-dom";
 import ExamRenderer from "../../components/ExamRenderer";
+import { Exam } from "../../types";
 
 export default function Results() {
   const location = useLocation();
-  const [examData, setExamData] = useState<any>(null);
+  const [exam, setExam] = useState<Exam>();
 
   useEffect(() => {
     const receivedExamData = location.state?.data;
 
     if (receivedExamData) {
-      setExamData(receivedExamData);
+      setExam(receivedExamData);
     }
   }, [location.state]);
 
@@ -21,9 +22,7 @@ export default function Results() {
       <Navbar />
       <section className="pt-24 flex flex-col items-center justify-center lg:px-48 md:px-12 px-4 bg-secondary">
         <h2 className="secondaryTitle bg-underline3 bg-100%">Prova gerada</h2>
-        <div className="p-4">
-          {examData && <ExamRenderer content={examData} />}
-        </div>
+        <div className="p-4">{exam && <ExamRenderer {...exam} />}</div>
       </section>
       <Footer />
     </div>
