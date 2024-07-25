39e82f813bf7b02ed8384af356e5aa77f3d4cd76
Mateus Elias <meap@cin.ufpe.br>
2024-07-25 07:00:33 -0300

    feat(backend/app.py): update generate exam endpoint

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from openai import OpenAI
 from utils import generate_prompt, generate_exam
+import json
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -16,28 +17,28 @@ def hello_world():
 def receive_data():
     data = request.get_json()
     num_questions = data.get("number_of_questions", None)
+    primary_instructions = data.get("primary_instructions", None)
     question_type = data.get("questions_type", None)
     subject = data.get("subject", None)
     difficulty = data.get("difficulty", None)
-    additional_info = data.get("additional_information", None)
     context_restriction = data.get("context_restriction")
     context_file = data.get("context", None)
 
     client = OpenAI(api_key=data.get("api_key"))
     prompt = generate_prompt(
         num_questions,
+        primary_instructions,
         question_type,
         subject,
         difficulty,
-        additional_info,
         context_file,
         context_restriction,
     )
-    exam = generate_exam(client, prompt)
+    exam = generate_exam(client, prompt, question_type)
+    exam = json.loads(exam)
 
     try:
         return jsonify({"message": "Exam generated successfully!", "exam": exam})
-
     except ValueError as ve:
         return jsonify({"error": str(ve)}), 500
 
